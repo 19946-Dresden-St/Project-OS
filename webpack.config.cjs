e989d0bda260754573322f56711060f71e6724ee
Izïa <izia.crinier@gmail.com>
2023-03-01 15:47:45 +0100

    fix dist into public

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'development',
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
